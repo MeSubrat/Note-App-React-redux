9952a4c0e730a1c4c73c7c49d70fe0709c1e3fdf
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2025-02-13 18:36:06 +0530

    .

--- src/store/NoteSlice.js
+++ src/store/NoteSlice.js
@@ -3,40 +3,40 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 const noteSlice = createSlice({
     name: 'notes',
     initialState: {
-        notes: JSON.parse(localStorage.getItem('notes'))||[],
-            // [
-            //     // {
-            //     //     id: nanoid(),
-            //     //     date: new Date().toLocaleString('hi-IN'),
-            //     //     title: "My first Note",
-            //     //     description: "Hey this is my first note.",
-            //     //     isEdited: false
-            //     // },
-            //     // {
-            //     //     id: nanoid(),
-            //     //     date: new Date().toLocaleString('hi-IN'),
-            //     //     title: "Shopping List",
-            //     //     description: "Buy milk, eggs, and bread."
-            //     // },
-            //     // {
-            //     //     id: nanoid(),
-            //     //     date: new Date().toLocaleString('hi-IN'),
-            //     //     title: "Meeting Reminder",
-            //     //     description: "Team meeting at 3 PM tomorrow."
-            //     // },
-            //     // {
-            //     //     id: nanoid(),
-            //     //     date: new Date().toLocaleString('hi-IN'),
-            //     //     title: "Project Ideas",
-            //     //     description: "AI-powered chatbot and quiz generator."
-            //     // },
-            //     // {
-            //     //     id: nanoid(),
-            //     //     date: new Date().toLocaleString('hi-IN'),
-            //     //     title: "Workout Plan",
-            //     //     description: "Morning jog and evening gym session."
-            //     // }
-            // ],
+        notes: JSON.parse(localStorage.getItem('notes')) || [],
+        // [
+        //     // {
+        //     //     id: nanoid(),
+        //     //     date: new Date().toLocaleString('hi-IN'),
+        //     //     title: "My first Note",
+        //     //     description: "Hey this is my first note.",
+        //     //     isEdited: false
+        //     // },
+        //     // {
+        //     //     id: nanoid(),
+        //     //     date: new Date().toLocaleString('hi-IN'),
+        //     //     title: "Shopping List",
+        //     //     description: "Buy milk, eggs, and bread."
+        //     // },
+        //     // {
+        //     //     id: nanoid(),
+        //     //     date: new Date().toLocaleString('hi-IN'),
+        //     //     title: "Meeting Reminder",
+        //     //     description: "Team meeting at 3 PM tomorrow."
+        //     // },
+        //     // {
+        //     //     id: nanoid(),
+        //     //     date: new Date().toLocaleString('hi-IN'),
+        //     //     title: "Project Ideas",
+        //     //     description: "AI-powered chatbot and quiz generator."
+        //     // },
+        //     // {
+        //     //     id: nanoid(),
+        //     //     date: new Date().toLocaleString('hi-IN'),
+        //     //     title: "Workout Plan",
+        //     //     description: "Morning jog and evening gym session."
+        //     // }
+        // ],
 
         dummyNotes: []
     },
@@ -49,10 +49,13 @@ const noteSlice = createSlice({
                 description: action.payload.description,
             }
             state.notes.push(note);
+            // localStorage.setItem('notes', JSON.stringify(state.notes));
         },
         removeNote: (state, action) => {
             state.notes = state.notes.filter((note) => note.id !== action.payload)
             state.dummyNotes = state.dummyNotes.filter(note => note.id !== action.payload);
+
+            localStorage.setItem('notes', JSON.stringify(state.notes));
         },
         updateNote: (state, action) => {
             const acceptedId = action.payload.id
@@ -71,6 +74,7 @@ const noteSlice = createSlice({
                 note.isEdited = true
                 console.log(note.isEdited)
             }
+            // localStorage.setItem('notes', JSON.stringify(state.notes));
         },
         searchNote: (state, action) => {
             const inputText = action.payload.toLowerCase()
